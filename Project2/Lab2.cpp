d23cec46c294cc627a41ee569166087288cd295b
cjtol <ctoliver@mail.umw.edu>
2026-05-20 18:55:54 -0400

    Basic Allegro Window created

---
+++ Project2/Lab2.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+
+int main(int argc, char** argv)
+{
+
+    ALLEGRO_DISPLAY* display = NULL;
+
+    if (!al_init())
+    {
+        fprintf(stderr, "failed to initialize allegro!\n");
+        return -1;
+    }
+    int width = 800;
+    int height = 600;
+    display = al_create_display(width, height);
+
+    if (!display)
+    {
+        fprintf(stderr, "failed to create display!\n");
+        return -1;
+    }
+
+
+    al_clear_to_color(al_map_rgb(0, 0, 0));
+    al_flip_display();
+
+    al_rest(5.0);
+
+    al_destroy_display(display);
+
+    return 0;
+}
