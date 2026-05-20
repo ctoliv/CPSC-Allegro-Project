634b15e3946f86ed1bf60c8c2e60dcbed03575b9
cjtol <ctoliver@mail.umw.edu>
2026-05-19 20:09:04 -0400

    Finished full house body and added tree

--- Project1/Lab1.cpp
+++ Project1/Lab1.cpp
@@ -35,17 +35,26 @@ int main(int argc, char** argv)
     al_draw_filled_triangle(250, 300, 550, 300, 400, 190, al_map_rgb(120, 30, 30));
     //Door
     al_draw_filled_rectangle(370, 390, 430, 500, al_map_rgb(90, 50, 25));
-
+    al_draw_filled_circle(420, 445, 5, al_map_rgb(230, 200, 60));
     //Left widow
     al_draw_filled_rectangle(285, 340, 345, 400, al_map_rgb(90, 50, 25));
     al_draw_rectangle(285, 340, 345, 400, al_map_rgb(0, 0, 0), 3);
+    al_draw_line(315, 340, 315, 400, al_map_rgb(0, 0, 0), 2);
+    al_draw_line(285, 370, 345, 370, al_map_rgb(0, 0, 0), 2);
 
     //Rigt window
     al_draw_filled_rectangle(455, 340, 515, 400, al_map_rgb(90, 50, 25));
     al_draw_rectangle(455, 340, 515, 400, al_map_rgb(0, 0, 0), 3);
+    al_draw_line(485, 340, 485, 400, al_map_rgb(0, 0, 0), 2);
+    al_draw_line(455, 370, 515, 370, al_map_rgb(0, 0, 0), 2);
 
+    //Tree trunk
+    al_draw_filled_rectangle(110, 350, 150, 500, al_map_rgb(100, 55, 25));
 
-
+    //Tree leaves
+    al_draw_filled_circle(130, 290, 50, al_map_rgb(25, 100, 30));
+    al_draw_filled_circle(90, 350, 50, al_map_rgb(30, 120, 40));
+    al_draw_filled_circle(170, 350, 50, al_map_rgb(30, 130, 40));
 
 
 
